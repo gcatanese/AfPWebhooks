a2fcc15b48b778bc079534a34d89cab078506490
gcatanese <gcatanese@yahoo.com>
2023-04-13 16:48:09 +0200

    Update JSON payload

--- src/test/java/com/adyen/examples/util/HMACValidatorTest.groovy
+++ src/test/java/com/adyen/examples/util/HMACValidatorTest.groovy
@@ -10,25 +10,47 @@ class HMACValidatorTest extends spock.lang.Specification {
     def "CalculateHMAC"() {
         given:
         def key = "6D5BADA576A73109D879220DCB793FFD67DEF7AA18C74CCC0AB66FD87AC8AEEA"
-        def payload = "{\"data\":{\"balancePlatform\":\"YourBalancePlatform\",\"creationDate\":\"2022-11-21T16:48:35+01:00\",\"id\":\"3JERI45WZHNCUHZY\",\"accountHolder\":{\"description\":\"Farah's Fedoras Company Account Holder\",\"id\":\"AH3227C223222C5GXTQM35ZX3\"},\"amount\":{\"currency\":\"EUR\",\"value\":900},\"balanceAccount\":{\"description\":\"Farah's Fedoras Balance Account\",\"id\":\"BA32272223222C5GXTQM43WKF\"},\"description\":\"Porcelain Doll: Eliza (20cm)\",\"originalAmount\":{\"currency\":\"EUR\",\"value\":900},\"platformPayment\":{\"account\":\"BA32272223222C5GXTQM43WKF\",\"modificationMerchantReference\":\"<auto>\",\"modificationPspReference\":\"G8X8VXGN2MQ7C782\",\"paymentMerchantReference\":\"uniqueReference\",\"paymentPspReference\":\"H42JQZ4RTGXXGN82\",\"reference\":\"6124145\",\"type\":\"BalanceAccount\"},\"reference\":\"6124145\",\"status\":\"Authorised\"},\"environment\":\"test\",\"type\":\"balancePlatform.payment.created\"}";
-        def final EXPECTED = "lFrZb+1R+3Hfnbh+VM4Jt5qZYre5r3Lu5RJeQQSsl6M="
+        def payload = "{\n" +
+                "    \"data\": {\n" +
+                "        \"balancePlatform\": \"myBalancePlatform\",\n" +
+                "        \"balanceAccount\": {\n" +
+                "            \"accountHolderId\": \"AH3227C223222B5CZW72CCC73\",\n" +
+                "            \"defaultCurrencyCode\": \"EUR\",\n" +
+                "            \"id\": \"BA3227C223222B5CZW72VCC75\",\n" +
+                "            \"status\": \"Active\"\n" +
+                "        }\n" +
+                "    },\n" +
+                "    \"environment\": \"test\",\n" +
+                "    \"type\": \"balancePlatform.balanceAccount.created\"\n" +
+                "}"
+        def final EXPECTED = "nvsZjQiHBuscSdtcA2cl1E+PSLJfgjPeRdd0pSaRiA0="
         when:
         def signature = hmacValidator.calculateHMAC(payload, key)
         then:
         signature == EXPECTED
-
     }
 
     def "ValidateHMAC"() {
         given:
-        def payload = "{\"data\":{\"balancePlatform\":\"YourBalancePlatform\",\"creationDate\":\"2022-11-21T16:48:35+01:00\",\"id\":\"3JERI45WZHNCUHZY\",\"accountHolder\":{\"description\":\"Farah's Fedoras Company Account Holder\",\"id\":\"AH3227C223222C5GXTQM35ZX3\"},\"amount\":{\"currency\":\"EUR\",\"value\":900},\"balanceAccount\":{\"description\":\"Farah's Fedoras Balance Account\",\"id\":\"BA32272223222C5GXTQM43WKF\"},\"description\":\"Porcelain Doll: Eliza (20cm)\",\"originalAmount\":{\"currency\":\"EUR\",\"value\":900},\"platformPayment\":{\"account\":\"BA32272223222C5GXTQM43WKF\",\"modificationMerchantReference\":\"<auto>\",\"modificationPspReference\":\"G8X8VXGN2MQ7C782\",\"paymentMerchantReference\":\"uniqueReference\",\"paymentPspReference\":\"H42JQZ4RTGXXGN82\",\"reference\":\"6124145\",\"type\":\"BalanceAccount\"},\"reference\":\"6124145\",\"status\":\"Authorised\"},\"environment\":\"test\",\"type\":\"balancePlatform.payment.created\"}";
+        def payload = "{\n" +
+                "    \"data\": {\n" +
+                "        \"balancePlatform\": \"myBalancePlatform\",\n" +
+                "        \"balanceAccount\": {\n" +
+                "            \"accountHolderId\": \"AH3227C223222B5CZW72CCC73\",\n" +
+                "            \"defaultCurrencyCode\": \"EUR\",\n" +
+                "            \"id\": \"BA3227C223222B5CZW72VCC75\",\n" +
+                "            \"status\": \"Active\"\n" +
+                "        }\n" +
+                "    },\n" +
+                "    \"environment\": \"test\",\n" +
+                "    \"type\": \"balancePlatform.balanceAccount.created\"\n" +
+                "}"
         def key = "6D5BADA576A73109D879220DCB793FFD67DEF7AA18C74CCC0AB66FD87AC8AEEA"
-        def final signature = "lFrZb+1R+3Hfnbh+VM4Jt5qZYre5r3Lu5RJeQQSsl6M="
+        def final signature = "nvsZjQiHBuscSdtcA2cl1E+PSLJfgjPeRdd0pSaRiA0="
         when:
         def valid = hmacValidator.validateHMAC(payload, signature, key)
         then:
         valid
-
     }
 
 }
